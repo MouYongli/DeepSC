f0b2f7301fdaee40265da17ab0911b4613b3da51
Ang Li <ang.li2@rwth-aachen.de>
2025-08-24 14:34:55 +0200

    Refactored cellxgene download

---
+++ scripts/download/01_download_cxg_run_download_cxg_index.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -a
+source .env
+set +a
+
+while read QUERY; do
+    echo "building index for ${QUERY}"
+    python -m deepsc.data.download.cellxgene.build_index_list --query-name ${QUERY} --output-dir ${DATA_PATH_CELLXGENE}
+done < ${QUERY_PATH_CELLXGENE}
