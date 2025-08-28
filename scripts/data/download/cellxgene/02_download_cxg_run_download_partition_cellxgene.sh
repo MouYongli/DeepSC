d0e9ec7269949392ef5d03eabed7f40519d9b29c
Ang Li <ang.li2@rwth-aachen.de>
2025-08-28 13:39:12 +0200

    enabled cellxgene download on warhol

--- scripts/data/download/cellxgene/02_download_cxg_run_download_partition_cellxgene.sh
+++ scripts/data/download/cellxgene/02_download_cxg_run_download_partition_cellxgene.sh
@@ -3,26 +3,22 @@ set -a
 source .env
 set +a
 
-QUERY_NAME=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$QUERY_PATH_CELLXGENE")
-INDEX_DIR="$INDEX_PATH_CELLXGENE"
-OUTPUT_DIR="$DATA_PATH_CELLXGENE"
+while read QUERY_NAME; do
+    INDEX_DIR="$INDEX_PATH_CELLXGENE"
+    OUTPUT_DIR="$DATA_PATH_CELLXGENE"
 
-echo "Downloading: ${QUERY_NAME}"
-echo "Index path: ${INDEX_DIR}"
-echo "Dataset path: ${OUTPUT_DIR}"
+    echo "Downloading: ${QUERY_NAME}"
+    MAX_PARTITION_SIZE=200000
+    TOTAL_NUM=$(wc -l "${INDEX_DIR}/${QUERY_NAME}.idx" | awk '{ print $1 }')
+    TOTAL_PARTITION=$((TOTAL_NUM / MAX_PARTITION_SIZE))
 
-MAX_PARTITION_SIZE=200000
-
-TOTAL_NUM=$(wc -l "${INDEX_DIR}/${QUERY_NAME}.idx" | awk '{ print $1 }')
-TOTAL_PARTITION=$((TOTAL_NUM / MAX_PARTITION_SIZE))
-
-for i in $(seq 0 $TOTAL_PARTITION)
-do
-    echo "Downloading ${QUERY_NAME} 的 partition ${i}/${TOTAL_PARTITION}"
-    python -m deepsc.data.download.cellxgene.download_partition \
-        --query-name "${QUERY_NAME}" \
-        --index-dir "${INDEX_DIR}" \
-        --output-dir "${OUTPUT_DIR}" \
-        --partition-idx "${i}" \
-        --max-partition-size "${MAX_PARTITION_SIZE}"
-done
+    for i in $(seq 0 $TOTAL_PARTITION); do
+        echo "Downloading ${QUERY_NAME} partition ${i}/${TOTAL_PARTITION}"
+        python -m deepsc.data.download.cellxgene.download_partition \
+            --query-name "${QUERY_NAME}" \
+            --index-dir "${INDEX_DIR}" \
+            --output-dir "${OUTPUT_DIR}" \
+            --partition-idx "${i}" \
+            --max-partition-size "${MAX_PARTITION_SIZE}"
+    done
+done < ${QUERY_PATH_CELLXGENE}
