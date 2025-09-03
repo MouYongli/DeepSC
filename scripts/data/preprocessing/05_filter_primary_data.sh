8456221ecaac1d36ae23c0ccd64fdf013f31115a
Ang Li <ang.li2@rwth-aachen.de>
2025-09-03 11:06:13 +0200

    Updated env settings

--- scripts/data/preprocessing/05_filter_primary_data.sh
+++ scripts/data/preprocessing/05_filter_primary_data.sh
@@ -5,6 +5,6 @@ source .env
 set +a
 
 python -m deepsc.data.preprocessing.filter_primary_data \
-    --NUM_WORKERS 32 \
-    --CELLXGENE_DIR $DATA_PATH_CELLXGENE \
-    --OUTPUT_BASE $DEEPSC_CELLXGENE_PRIMARY_DATA_PATH
+    --num_workers 32 \
+    --cellxgene_dir $DATA_PATH_CELLXGENE \
+    --output_base $DEEPSC_CELLXGENE_PRIMARY_DATA_PATH
