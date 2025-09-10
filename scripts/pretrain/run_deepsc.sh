58249c51094d37873a46e679478303e8b83a723e
Yongli Mou <mouyongli@hotmail.com>
2025-09-10 09:20:24 +0200

    Update scripts structure for pretrain

--- scripts/pretrain/run_deepsc.sh
+++ scripts/pretrain/run_deepsc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/zsh
 
-# 使用物理编号为 2 和 3 的 GP
+# Use GPU/GPUs on one machine
+# Here the example is using GPUs with physical numbers 2 and 3
 export CUDA_VISIBLE_DEVICES=1,2
 export OMP_NUM_THREADS=64
 
@@ -10,4 +11,5 @@ MASTER_PORT=12620
 torchrun \
   --nproc_per_node=$NUM_GPUS \
   --master_port=$MASTER_PORT \
-  -m deepsc.pretrain.pretrain
+  -m deepsc.pretrain.pretrain \
+  --model deepsc
